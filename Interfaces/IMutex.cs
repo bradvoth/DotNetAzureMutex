1936f43d08e5289e54e45043549ce975cf5c479b
bradvoth <brad@voth.name>
2021-07-08 12:53:12 -0400

    Add documentation to interface (#6)

--- Interfaces/IMutex.cs
+++ Interfaces/IMutex.cs
@@ -5,8 +5,24 @@ namespace Interfaces
 {
     public interface IMutex : IDisposable
     {
+        /// <summary>
+        /// Acquire an exclusive lock for seconds duration
+        /// </summary>
+        /// <param name="duration">seconds</param>
+        /// <returns></returns>
         public Task Lock(int duration);
+        
+        /// <summary>
+        /// Renew a lock for an additional seconds duration
+        /// </summary>
+        /// <param name="duration">seconds</param>
+        /// <returns></returns>
         public Task Renew(int duration);
+        
+        /// <summary>
+        /// Release a given lock
+        /// </summary>
+        /// <returns></returns>
         public Task Release();
     }
 }
